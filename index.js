a17888f04217759e4d0bea79d7fefb5799339180
Andrius Skerla <andrius.skerla@gmail.com>
2016-09-08 12:31:18 +0100

    hello world

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const co = require('co');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  read
+};
+
+/**
+ *
+ * @param dir {String}
+ * @param options {{filter, directory_filter}}
+ * @returns {*}
+ */
+function read(dir, options = {}) {
+  const noop_filter = () => true;
+  const filter = options.filter || noop_filter;
+  const directory_filter = options.directory_filter || noop_filter;
+
+  return co(go);
+
+  /**
+   *
+   * @returns {Array}
+   */
+  function *go() {
+    const result = [];
+    const files = [path.resolve(dir)];
+
+    for (let file of files) {
+      const stat = yield cb => fs.stat(file, cb);
+
+      if (stat.isDirectory()) {
+        yield processDirectory(files, file)
+        continue;
+      }
+
+      if (filter(file) === false) {
+        continue;
+      }
+
+      result.push(file);
+    }
+
+    return result;
+  }
+
+  /**
+   *
+   * @param files
+   * @param file
+   */
+  function *processDirectory(files, file) {
+    if (directory_filter(file) === false) {
+      return;
+    }
+
+    const newFiles = yield cb => fs.readdir(file, cb);
+    Array.prototype.push.apply(files, newFiles.map(item => path.resolve(file, item)));
+  }
+}
