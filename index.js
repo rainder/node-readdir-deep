201de595375979a0bbaed1094fea4b65dc257433
Andrius Skerla <andrius@skerla.com>
2016-09-14 17:53:34 +0100

    +readSync

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-  read
+  read,
+  readSync
 };
 
 /**
@@ -58,6 +59,54 @@ function read(dir, options = {}) {
     }
 
     const newFiles = yield cb => fs.readdir(file, cb);
+    Array.prototype.push.apply(files, newFiles.map(item => path.resolve(file, item)));
+  }
+}
+
+function readSync(dir, options = {}) {
+  const noop_filter = () => true;
+  const filter = options.filter || noop_filter;
+  const directory_filter = options.directory_filter || noop_filter;
+
+  return go();
+
+  /**
+   *
+   * @returns {Array}
+   */
+  function go() {
+    const result = [];
+    const files = [path.resolve(dir)];
+
+    for (let file of files) {
+      const stat = fs.statSync(file);
+
+      if (stat.isDirectory()) {
+        processDirectory(files, file)
+        continue;
+      }
+
+      if (filter(file) === false) {
+        continue;
+      }
+
+      result.push(file);
+    }
+
+    return result;
+  }
+
+  /**
+   *
+   * @param files
+   * @param file
+   */
+  function processDirectory(files, file) {
+    if (directory_filter(file) === false) {
+      return;
+    }
+
+    const newFiles = fs.readdirSync(file);
     Array.prototype.push.apply(files, newFiles.map(item => path.resolve(file, item)));
   }
 }
